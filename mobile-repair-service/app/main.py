a4ff9aef2e344c2556fdc1c7e0a31efbc6fe34f6
KadriSof <kadrisofyen@gmail.com>
2024-05-13 18:24:07 +0100

    Mobile Repair Service App

--- mobile-repair-service/app/main.py
+++ mobile-repair-service/app/main.py
@@ -9,21 +9,25 @@ from screens.home_screen import HomeScreen
 
 
 class MainApp(MDApp):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.sm = ScreenManager()
+
     def build(self):
         # self.theme_cls.theme_style_switch_animation = True
         self.theme_cls.theme_style = 'Dark'
-        self.theme_cls.primary_palette = 'BlueGray'
+        self.theme_cls.primary_palette = 'Indigo'
 
 
         # Load screen definitions from .kv files
         Builder.load_file("screens/login_screen.kv")
         Builder.load_file("screens/home_screen.kv")
 
-        sm = ScreenManager()
-        sm.add_widget(LoginScreen(name='login_screen'))
-        sm.add_widget(HomeScreen(name='home_screen'))
+        # self.sm = ScreenManager()
+        self.sm.add_widget(LoginScreen(name='login_screen'))
+        self.sm.add_widget(HomeScreen(name='home_screen'))
 
-        return sm
+        return self.sm
 
     def on_start(self) -> None:
         logging.info('MainApp: Application started!')
